870f3f957c7a3dc06104fb87c5a3580e0f292b7d
sundalei <sundalei2011@163.com>
2018-04-20 19:57:26 +0800

    bug fixed

--- src/packt/jee/book/git/GitTest.java
+++ src/packt/jee/book/git/GitTest.java
@@ -2,7 +2,7 @@ package packt.jee.book.git;
 
 public class GitTest {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello world, from branch #1234!!");
 		System.out.println("welcome");
 	}
 }
